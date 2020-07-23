3f5ae6e43e99e5267abfe1a5142c74218694c6e1
Walter <walteraandrade@gmail.com>
2020-07-23 16:11:16 -0300

    .

--- src/routes/postRouter.ts
+++ src/routes/postRouter.ts
@@ -1,4 +1,4 @@
-import express,{Request, Response} from "express";
+import express, { Request, Response } from "express";
 import { create } from "../business/controller/PostController";
 import { Authenticator } from "../services/Authenticator";
 import { PostDatabase } from "../data/PostDatabase";
@@ -6,36 +6,35 @@ import moment from "moment";
 import { BaseDatabase } from "../data/BaseDatabase";
 import { create, getPostByType } from "../business/controller/PostController";
 
-
 export const PostRouter = express.Router();
 
 PostRouter.post("/create", create);
 
+PostRouter.get(
+  "/feed",
+  async (req: Request, res: Response): Promise<any> => {
+    try {
+      const authenticator = new Authenticator();
+      authenticator.getData(req.headers.authorization!);
 
-PostRouter.get("/feed", async (req: Request, res: Response): Promise<any> => {
-  try {
-    const authenticator = new Authenticator()
-    authenticator.getData(req.headers.authorization!)
+      const postDb = new PostDatabase();
+      const postFeed = await postDb.getPosts();
 
-    const postDb = new PostDatabase()
-    const postFeed = await postDb.getPosts()
-    
-    const feeds = postFeed.map((feed: any) => {
-      feed.creation_date = moment(feed.creation_date).format("DD/MM/YYYY")
-      return feed
-    })
-    
-  
-    res.status(200).send({
-      feeds
-    })
-  } catch (err) {
-    res.status(400).send({
-      message: err.message
-    })
+      const feeds = postFeed.map((feed: any) => {
+        feed.creation_date = moment(feed.creation_date).format("DD/MM/YYYY");
+        return feed;
+      });
+
+      res.status(200).send({
+        feeds,
+      });
+    } catch (err) {
+      res.status(400).send({
+        message: err.message,
+      });
+    }
+    await BaseDatabase.destroyConnection();
   }
-  await BaseDatabase.destroyConnection()
-})
+);
 
-PostRouter.get("/", getPostByType)
-
+PostRouter.get("/", getPostByType);
